4886145a533431a84feb13a7ea291541fcf61a52
LECOQ Vincent <vincent.lecoq@gmail.com>
2025-11-19 16:52:58 +0900

    improve sprite detection
    Merge small detection included in a bigger one
    Use temporary detectionBoxes list before build atlas

--- SpriteStudio/src/spriteextractor.cpp
+++ SpriteStudio/src/spriteextractor.cpp
@@ -30,6 +30,7 @@ QList<QPixmap> SpriteExtractor::extractFrames(const QString &filePath)
 
     QVector<QVector<bool>> visited(h, QVector<bool>(w, false));
     m_atlas = atlasPixmap;
+    QList<Extractor::Box> detectedBoxes;
     for (int y = 0; y < h; ++y) {
         for (int x = 0; x < w; ++x) {
             if (!visited[y][x] && qAlpha(image.pixel(x, y)) > 0) {
@@ -73,13 +74,80 @@ QList<QPixmap> SpriteExtractor::extractFrames(const QString &filePath)
                     int spriteH = maxY - minY + 1;
 
                     Extractor::Box box = {minX, minY, spriteW, spriteH};
-                    m_atlas_index.push_back(box);
-
-                    QPixmap spriteFrame = atlasPixmap.copy(minX, minY, spriteW, spriteH);
-                    this->addFrame(spriteFrame);
+                    detectedBoxes.push_back(box);
                 }
             }
         }
+    }
+
+    if (detectedBoxes.size() > 1) {
+        QList<QRect> rects;
+        for (const auto& box : detectedBoxes) {
+            rects.append(QRect(box.x, box.y, box.w, box.h));
+        }
+
+        QList<QRect> finalRects;
+        QList<bool> mergedFlags(rects.size(), false);
+
+        for (int i = 0; i < rects.size(); ++i) {
+            if (mergedFlags[i]) continue;
+
+            QRect currentRect = rects[i];
+            bool isSatellite = false;
+            int containingIndex = -1;
+
+            for (int j = 0; j < rects.size(); ++j) {
+                if (i == j || mergedFlags[j]) continue;
+
+                QRect otherRect = rects[j];
+
+                if (otherRect.contains(currentRect)) {
+                    isSatellite = true;
+                    containingIndex = j;
+                    break;
+                }
+            }
+
+            if (isSatellite) {
+                mergedFlags[i] = true;
+            } else {
+                finalRects.append(currentRect);
+                mergedFlags[i] = true;
+            }
+        }
+
+        QList<QRect> masterRects;
+        QList<bool> isMaster(rects.size(), true);
+
+        for (int i = 0; i < rects.size(); ++i) {
+            for (int j = 0; j < rects.size(); ++j) {
+                if (i == j) continue;
+
+                if (rects[j].contains(rects[i])) {
+                    isMaster[i] = false;
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < rects.size(); ++i) {
+            if (isMaster[i]) {
+                masterRects.append(rects[i]);
+            }
+        }
+
+        m_atlas_index.clear();
+        for (const auto& rect : masterRects) {
+            m_atlas_index.push_back({rect.x(), rect.y(), rect.width(), rect.height()});
+        }
+
+    } else if (detectedBoxes.size() == 1) {
+        m_atlas_index = detectedBoxes;
+    }
+
+    for (const auto& box : m_atlas_index) {
+        QPixmap spriteFrame = atlasPixmap.copy(box.x, box.y, box.w, box.h);
+        this->addFrame(spriteFrame);
     }
 
     qDebug() << "Extraction du sprite sheet terminée. Frames trouvées:" << m_frames.size();
