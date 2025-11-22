7bbea8a21042a5aa59c785c6a64b728be7c8bf9d
LECOQ Vincent <110967411+oktailb@users.noreply.github.com>
2025-11-22 16:11:58 +0900

    Fix probable crash due to uninitialised extractor

--- build.cmake
+++ build.cmake
@@ -4,5 +4,5 @@ if(WIN32)
 endif(WIN32)
 
 #set(CMAKE_BUILD_TYPE Release)
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ggdb -W -Wall -pedantic")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ggdb -W -Wall -pedantic")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O5 -W -Wall -pedantic")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O5 -W -Wall -pedantic")
